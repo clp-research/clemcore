3d7960211c388fa3fa6480d038762e3bc89f2950
Lara Pfennigschmidt <pfennigschmi@uni-potsdam.de>
2023-12-19 15:32:16 +0000

    Added type annotations, fixed prompt substitution of sets, fixed some validation errors, fixed gameplay aborting on failed guesser validation

--- games/codenames/master.py
+++ games/codenames/master.py
@@ -8,25 +8,31 @@ from clemgame import get_logger
 logger = get_logger(__name__)
 
 GAME_NAME = "codenames"
+SEED = 418
+
 # TODO: all words on board in CAPS?
+# TODO: reprompting on failed response validation
+# TODO: scoring
+# TODO: self-logging of some actions for scoring later?
+# TODO: reuse players for other codename variants, e.g. Duet?
+# TODO: intermittent prompts e.g. "The next clue is:<CLUE>. The list of words now is: <List of words>"
 
 class Guesser(Player):
-    def __init__(self):
-        super().__init__("programmatic")
-        self.guesses = ['apple', 'banana']
-        self.prefix = "GUESS: "
+    def __init__(self, model_name: str):
+        super().__init__(model_name)
+        self.guesses: List[str] = ['apple', 'banana']
+        self.prefix: str = "GUESS: "
 
-    def _custom_response(self, history, turn):
+    def _custom_response(self, history, turn) -> str:
         # raise NotImplementedError("This should not be called, but the remote APIs.")
         # TODO: randomly guess number of words from the board
         prompt = history[-1]["content"]
-        board = prompt.split('\n\n')[1].strip('{}').split(', ')
-        board = [word.strip("'") for word in board]
+        board = prompt.split('\n\n')[1].split(', ')
         number_of_allowed_guesses = int(re.search(r"Please select up to ([0-9]+) words", prompt).group(1))
         self.guesses = random.sample(board, number_of_allowed_guesses)
         return self.recover_utterance()
     
-    def validate_response(self, utterance, board, number_of_allowed_guesses):
+    def validate_response(self, utterance: str, board: Set[str], number_of_allowed_guesses: int) -> bool:
         # all lines need to start with GUESS
         lines = utterance.split("\n")
         for line in lines:
@@ -46,43 +52,45 @@ class Guesser(Player):
             
         return True
 
-    def parse_response(self, utterance):
+    def parse_response(self, utterance: str) -> str:
         lines = utterance.split("\n")
         self.guesses = [line[len(self.prefix):] for line in lines]
-        return self.guesses
+        return ", ".join(self.guesses)
             
-    def recover_utterance(self):
+    def recover_utterance(self) -> str:
         with_prefix = [self.prefix + guess for guess in self.guesses]
         return "\n".join(with_prefix)
 
 
 class ClueGiver(Player):
-    def __init__(self):
-        super().__init__("programmatic")
+    def __init__(self, model_name: str):
+        super().__init__(model_name)
         self.prefix: str = "CLUE: "
         self.clue: str = 'fruit'
         self.number_of_targets: int = 2
         self.targets: List[str] = ['banana', 'apple']
 
-    def _custom_response(self, history, turn):
+    def _custom_response(self, history, turn) -> str:
         #raise NotImplementedError("This should not be called, but the remote APIs.")
         # TODO: random clue giving? how?
         prompt = history[-1]["content"]
-        team_words = re.search(r"Your team words are: \{(.*)\}", prompt).group(1)
+        team_words = re.search(r"Your team words are: (.*)", prompt).group(1)
         team_words = team_words.split(', ')
-        team_words = [word.strip("'") for word in team_words]
         self.targets = random.sample(team_words, min(2, len(team_words)))
         self.number_of_targets = len(self.targets)
         self.clue = "".join(random.sample(list(string.ascii_lowercase), 6))
         return self.recover_utterance()
     
-    def validate_response(self, utterance, board):
+    def validate_response(self, utterance: str, board: Set[str]) -> bool:
         # needs to start with correct prefix
         if not utterance.startswith(self.prefix):
             print(f"Utterance {utterance} did not start with the correct prefix. ({self.prefix})")
             return False
         utterance = utterance[len(self.prefix):]
         parts = utterance.split(', ')
+        if len(parts) < 3:
+            print(f"Utterance did not contain enough parts {len(parts)} of required clue, number, and targets (at least three comma-separated)")
+            return False
         clue = parts[0]
         number_of_targets = parts[1]
         targets = parts[2:]
@@ -98,7 +106,7 @@ class ClueGiver(Player):
             return False
         
         # Number needs to be a valid number and in between a valid range of 1 and all words on the board (alternatively only team words?)
-        if not number_of_targets.isdigit() and not (0 < int(number_of_targets) <= len(board)):
+        if not number_of_targets.isdigit() or not (0 < int(number_of_targets) <= len(board)):
             print(f"Number {number_of_targets} is not within range of 0 and {len(board)}")
             return False
         number_of_targets = int(number_of_targets)
@@ -114,7 +122,7 @@ class ClueGiver(Player):
             
         return True
 
-    def parse_response(self, utterance):
+    def parse_response(self, utterance) -> str:
         utterance = utterance[len(self.prefix):]
         parts = utterance.split(', ')
         self.clue = parts[0]
@@ -122,7 +130,7 @@ class ClueGiver(Player):
         self.targets = parts[2:]
         return f"{self.clue}, {self.number_of_targets}"
 
-    def recover_utterance(self):
+    def recover_utterance(self) -> str:
         return f"{self.prefix}{self.clue}, {self.number_of_targets}, {', '.join(self.targets)}"
 
 
@@ -138,21 +146,15 @@ class CodenamesGame(DialogueGameMaster):
         self.experiment: str = experiment["name"]
         self.experiment_type: str = experiment["type"]
         
-        self.aborted: bool = False
-        self.lose: bool = False
+        #self.aborted: bool = False
+        #self.lose: bool = False
+        self.invalid_response: bool = False
 
         # save player interfaces
-        self.model_a = player_backends[0]
-        self.model_b = player_backends[1]
+        self.model_a: str = player_backends[0]
+        self.model_b: str = player_backends[1]
         
     def _on_setup(self, **game_instance):
-        """
-        Template method: must be implemented
-
-        Use add_player() here to add the players.
-
-        :param kwargs: of the game instance
-        """
         self.game_instance = game_instance  # fetch game parameters here
         self.board: Set[str] = set(game_instance["board"])
         self.team_words: Set[str] = set(game_instance["assignments"]["team"])
@@ -162,23 +164,42 @@ class CodenamesGame(DialogueGameMaster):
         self.uncovered_words: Set[str] = set()
 
         # Create the players
-        self.cluegiver = ClueGiver()#game_instance["target_name"])
-        self.guesser = Guesser()#self.player_backends[0])
+        self.cluegiver: Player = ClueGiver(self.player_backends[0])
+        self.guesser: Player = Guesser(self.player_backends[1])
 
         # Add the players: these will be logged to the records interactions.json
         # Note: During game play the players will be called in the order added here
         self.add_player(self.cluegiver)
         self.add_player(self.guesser)
 
+    def _get_cluegiver_prompt(self) -> str:
+        prompt_cluegiver = self.load_template("resources/initial_prompts/prompt_cluegiver")
+        team_words = ", ".join(list(self.team_words - self.uncovered_words))
+        opponent_words = ", ".join(list(self.opponent_words - self.uncovered_words))
+        innocent_words = ", ".join(list(self.innocent_words - self.uncovered_words))
+        assassin_words = ", ".join(list(self.assassin_words - self.uncovered_words))
+        instance_prompt_cluegiver = Template(prompt_cluegiver).substitute(team_words= team_words, 
+                                                                          opponent_words=opponent_words, 
+                                                                          innocent_words=innocent_words, 
+                                                                          assassin_words=assassin_words)
+        return instance_prompt_cluegiver
+    
+    def _get_guesser_prompt(self) -> str:
+        prompt_guesser = self.load_template("resources/initial_prompts/prompt_guesser")
+        board = ", ".join(list(self.board - self.uncovered_words))
+        instance_prompt_guesser = Template(prompt_guesser).substitute(board=board, 
+                                                                      clue=self.cluegiver.clue, 
+                                                                      number=self.cluegiver.number_of_targets)
+        return instance_prompt_guesser
+
     def _on_before_game(self):
         # Do something before the game start e.g. add the initial prompts to the message list for the players
-        # Load the prepared initial prompt
-        prompt_cluegiver = self.load_template("resources/initial_prompts/prompt_cluegiver")
-        instance_prompt_cluegiver = Template(prompt_cluegiver).substitute(team_words=self.team_words, opponent_words=self.opponent_words, innocent_words=self.innocent_words, assassin_words=self.assassin_words)
-        self.add_user_message(self.cluegiver, instance_prompt_cluegiver)
+        self.add_user_message(self.cluegiver, self._get_cluegiver_prompt())
 
-    def _does_game_proceed(self):
+    def _does_game_proceed(self) -> bool:
         # Determine if the game should proceed. This is also called once initially.
+        if self.invalid_response:
+            return False
         
         # for the base version, a check is needed whether all team words from one team are revealed or the assassin is revealed
         if len(self.uncovered_words.intersection(self.team_words)) == len(self.team_words):
@@ -195,17 +216,6 @@ class CodenamesGame(DialogueGameMaster):
         return True
 
     def _on_parse_response(self, player: Player, utterance: str) -> Tuple[str, bool]:
-        """
-        Hook
-
-        Decide if a response utterance should be modified. If not simply return the utterance.
-
-        When a modified utterance and a true value is returned, then a 'parse' event is logged.
-
-        :param player: that produced the response
-        :param utterance: to be potentially modified
-        :return: the (modified) utterance and if to log the parse action (default: True)
-        """
         if player == self.cluegiver:
             return player.parse_response(utterance), True
         else:
@@ -214,69 +224,48 @@ class CodenamesGame(DialogueGameMaster):
             return guesses, True
 
     def _validate_player_response(self, player: Player, utterance: str) -> bool:
-        """
-        Hook
-
-        Decide if an utterance should be added.
-
-        This is also the place to check for game end conditions.
-
-        :param player: for which the response is added as "assistant" to the history
-        :param utterance: to be added
-        :return: True, if the utterance is fine; False, if the response should not be added to the history
-        """
         if player == self.cluegiver:
-            return player.validate_response(utterance, self.board - self.uncovered_words)
+            valid_response = player.validate_response(utterance, self.board - self.uncovered_words)
         else:
-            return player.validate_response(utterance, self.board - self.uncovered_words, self.cluegiver.number_of_targets)
+            valid_response = player.validate_response(utterance, self.board - self.uncovered_words, self.cluegiver.number_of_targets)
+        self.invalid_response = not valid_response
+        return valid_response
     
     def _on_before_turn(self, current_turn):
-        # add new prompts?
-        prompt_cluegiver = self.load_template("resources/initial_prompts/prompt_cluegiver")
-        instance_prompt_cluegiver = Template(prompt_cluegiver).substitute(team_words=self.team_words - self.uncovered_words, 
-                                                                          opponent_words=self.opponent_words - self.uncovered_words, 
-                                                                          innocent_words=self.innocent_words - self.uncovered_words, 
-                                                                          assassin_words=self.assassin_words - self.uncovered_words)
-        self.add_user_message(self.cluegiver, instance_prompt_cluegiver)
+        # add new cluegiver prompt
+        self.add_user_message(self.cluegiver, self._get_cluegiver_prompt())
 
     def _on_after_turn(self, turn_idx: int):
         pass
     
     def _after_add_player_response(self, player: Player, utterance: str):
-        """
-        Hook
-
-        Add the utterance to other player's history, if necessary.
-        To do this use the method add_user_message(other_player,utterance).
-
-        :param player: that produced the response (or has been modified by the GM)
-        :param utterance: that has been added
-        """
         if player == self.cluegiver:
-            # put message into next prompt?
-            prompt_guesser = self.load_template("resources/initial_prompts/prompt_guesser")
-            instance_prompt_guesser = Template(prompt_guesser).substitute(board=self.board - self.uncovered_words, clue=self.cluegiver.clue, number=self.cluegiver.number_of_targets)
-            self.add_user_message(self.guesser, instance_prompt_guesser)
+            # put clue and number of targets into prompt for guesser
+            self.add_user_message(self.guesser, self._get_guesser_prompt())
         else:
             # TODO: use intermittent prompt, that also includes guess from guessing player and then prompts for new clue, with updated lists of board words
             self.add_user_message(self.cluegiver, utterance)
 
 
-    def compute_scores(self) -> None:
+    def compute_scores(self, episode_interactions: Dict) -> None:
         # metrics here for episode scores
         # where to put metrics for turns?
         score = 0
         if self.success:
             score = 1
         self.log_episode_score('Accuracy', score)
+        for turn_idx, turn in enumerate(episode_interactions["turns"]):
+            turn_score = {"guess": None, "clue": None, "request_count": 1}
+            pass
 
 
 class CodenamesGameBenchmark(GameBenchmark):
 
     def __init__(self):
         super().__init__(GAME_NAME)
+        random.seed(SEED)
 
-    def get_description(self):
+    def get_description(self) -> str:
         return "Codenames game between a cluegiver and a guesser"
 
     def create_game_master(self, experiment: Dict, player_backends: List[str]) -> GameMaster:
