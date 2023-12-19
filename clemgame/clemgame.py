99e19c572d4ae488cdc9db9b3eb3b9cfcebecac7
Lara Pfennigschmidt <pfennigschmi@uni-potsdam.de>
2023-12-19 10:35:15 +0000

    Fixing typos in framework

--- clemgame/clemgame.py
+++ clemgame/clemgame.py
@@ -342,7 +342,7 @@ class DialogueGameMaster(GameMaster):
         """
         Add a player to the game.
 
-        Note: The players will be called at the same order as added!
+        Note: The players will be called in the same order as added!
 
         :param player: to be added to the game
         """
