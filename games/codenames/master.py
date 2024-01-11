383ac1582e3fe039bf42f5e6ad487f28260e6824
Lara Pfennigschmidt <pfennigschmi@uni-potsdam.de>
2024-01-11 12:47:20 +0000

    Add scoring to gamemaster

--- games/codenames/master.py
+++ games/codenames/master.py
@@ -17,13 +17,12 @@ class ValidationError(Exception):
         super().__init__(self.message)
 
 # TODO: all words on board in CAPS?
-# TODO: scoring
-# TODO: self-logging of some actions for scoring later?
 # TODO: reuse players for other codename variants, e.g. Duet?
 # TODO: intermittent prompts e.g. "The next clue is:<CLUE>. The list of words now is: <List of words>"
 # TODO: only count first guess if it is wrong... no further guesses are counted
 # TODO: change prompts on reprompt, let them reflect the errors
 # TODO: ValidationError for "answer was too long, did not follow the specified format"
+# TODO: log target revealed, opponent card revealed, basically everything needed for scoring!
 
 class Guesser(Player):
     def __init__(self, model_name: str):
@@ -33,8 +32,6 @@ class Guesser(Player):
         self.retries: int = 0
 
     def _custom_response(self, history, turn) -> str:
-        # raise NotImplementedError("This should not be called, but the remote APIs.")
-        # TODO: randomly guess number of words from the board
         prompt = history[-1]["content"]
         board = prompt.split('\n\n')[1].split(', ')
         number_of_allowed_guesses = int(re.search(r"Please select up to ([0-9]+) words", prompt).group(1))
@@ -59,7 +56,7 @@ class Guesser(Player):
     def parse_response(self, utterance: str) -> str:
         lines = utterance.split("\n")
         self.guesses = [line[len(self.prefix):] for line in lines]
-        return ", ".join(self.guesses)
+        return self.guesses
             
     def recover_utterance(self) -> str:
         with_prefix = [self.prefix + guess for guess in self.guesses]
@@ -77,12 +74,16 @@ class ClueGiver(Player):
 
     def _custom_response(self, history, turn) -> str:
         prompt = history[-1]["content"]
-        team_words = re.search(r"Your team words are: (.*)", prompt).group(1)
-        team_words = team_words.split(', ')
+        # print("Prompt: ", prompt)
+        match = re.search(r"Your team words are: (.*)", prompt)
+        if match != None:
+            # Player was actually prompted (otherwise it was reprompted and the team_words stay the same)
+            team_words = match.group(1)
+            team_words = team_words.split(', ')
         self.targets = random.sample(team_words, min(2, len(team_words)))
         self.number_of_targets = len(self.targets)
         self.clue = "".join(random.sample(list(string.ascii_lowercase), 6))
-        return self.recover_utterance()
+        return self.recover_utterance(with_targets=True)
     
     def validate_response(self, utterance: str, board: Set[str]):
         # needs to start with correct prefix
@@ -91,7 +92,7 @@ class ClueGiver(Player):
         utterance = utterance[len(self.prefix):]
         parts = utterance.split(', ')
         if len(parts) < 3:
-            raise ValidationError(f"Utterance did not contain enough parts {len(parts)} of required clue, number, and targets (at least three comma-separated)")
+            raise ValidationError(f"Utterance {utterance} did not contain enough parts (only {len(parts)}) of required clue, number, and targets (at least three comma-separated)")
         clue = parts[0]
         number_of_targets = parts[1]
         targets = parts[2:]
@@ -116,7 +117,7 @@ class ClueGiver(Player):
             if not target in board:
                 raise ValidationError(f"Targeted word {target} does not exist on board.")
             
-    def parse_response(self, utterance) -> str:
+    def parse_response(self, utterance: str) -> str:
         utterance = utterance[len(self.prefix):]
         parts = utterance.split(', ')
         self.clue = parts[0]
@@ -223,6 +224,7 @@ class CodenamesGame(DialogueGameMaster):
             try:
                 player.validate_response(utterance, self.board - self.uncovered_words)
             except ValidationError as error:
+                print(error.message)
                 self.log_to_self("cluegiver validation error", error.message)
                 self.invalid_response = True
         else:
@@ -237,11 +239,28 @@ class CodenamesGame(DialogueGameMaster):
     def _on_parse_response(self, player: Player, utterance: str) -> Tuple[str, bool]:
         if player == self.cluegiver:
             self.log_to_self("clue", self.cluegiver.recover_utterance(with_targets = True) )
+            self.log_to_self("targets", self.cluegiver.targets)
             return player.parse_response(utterance), True
         else:
             self.log_to_self("guess", self.guesser.recover_utterance())
             guesses = player.parse_response(utterance)
             self.uncovered_words.update(guesses)
+            for guess in guesses:
+                print("Guess:", guess)
+                if guess in self.assassin_words:
+                    self.log_to_self("card uncovered", "assassin")
+                elif guess in self.innocent_words:
+                    self.log_to_self("card uncovered", "innocent")
+                elif guess in self.cluegiver.targets:
+                    self.log_to_self("card uncovered", "target")
+                elif guess in self.team_words:
+                    self.log_to_self("card uncovered", "team")
+                elif guess in self.opponent_words:
+                    self.log_to_self("card uncovered", "opponent")
+                else:
+                    self.log_to_self("error", f"Made guess {guess} does not reflect a card.")
+                    print("Made guess {guess} does not reflect a card, should not happen.")
+                
             return guesses, True
         
     def _on_before_reprompt(self, player: Player):
@@ -263,17 +282,81 @@ class CodenamesGame(DialogueGameMaster):
             # TODO: use intermittent prompt, that also includes guess from guessing player and then prompts for new clue, with updated lists of board words
             self.add_user_message(self.cluegiver, utterance)
 
-
     def compute_scores(self, episode_interactions: Dict) -> None:
-        # metrics here for episode scores
-        # where to put metrics for turns?
-        score = 0
-        if self.success:
-            score = 1
-        self.log_episode_score('Accuracy', score)
+        # turn_scores = []
+        # print(episode_interactions)
+        # TODO: something is still up with target precision and target recall, they do not match. e.g. precision is 0.5 but recall is 0.
+        episode_score = {"cluegiver invalid format": 0, "guesser invalid format": 0, 
+                         "cards uncovered" : {"target": 0, "team": 0, "opponent": 0, "innocent": 0, "assassin": 0, "total": 0},
+                         "number of turns" : 0}
         for turn_idx, turn in enumerate(episode_interactions["turns"]):
-            turn_score = {"guess": None, "clue": None, "request_count": 1}
-            pass
+            # Metrics per turn:
+            # target-precision, target-recall, target-f1
+            # team-precision
+            # invalid formats per player
+            turn_score = {"clue": None, "targets": [], "cluegiver invalid format": 0, 
+                          "guess": None, "guesser invalid format": 0, 
+                          "cards uncovered": {"target": 0, "team": 0, "opponent": 0, "innocent": 0, "assassin": 0, "total": 0}}
+            for event in turn:
+                action = event['action']
+                match action["type"]:
+                    case "cluegiver validation error":
+                        turn_score["cluegiver invalid format"] += 1
+                    case "guesser validation error":
+                        turn_score["guesser invalid format"] += 1
+                    case "clue" | "targets" | "guess ":
+                        turn_score[action["type"]] = action["content"]
+                    case "card uncovered":
+                        turn_score["cards uncovered"][action["content"]] += 1
+                        if action["content"] == "target":
+                            turn_score["cards uncovered"]["team"] += 1
+                        turn_score["cards uncovered"]["total"] += 1
+
+            sum_uncovered_cards = turn_score["cards uncovered"]["team"] + turn_score["cards uncovered"]["opponent"] + turn_score["cards uncovered"]["innocent"] + turn_score["cards uncovered"]["assassin"]
+            target_precision = 0
+            target_recall = 0
+            target_f1 = 0
+            team_precision = 0
+            if sum_uncovered_cards:
+                target_precision = turn_score["cards uncovered"]["target"] / sum_uncovered_cards
+                team_precision = turn_score["cards uncovered"]["team"] / sum_uncovered_cards
+            
+            if len(turn_score["targets"]) > 0:
+                target_recall = turn_score["cards uncovered"]["target"] / len(turn_score["targets"])
+            if target_precision + target_recall > 0:
+                target_f1 = 2 * target_precision * target_recall / (target_precision + target_recall)
+            
+            self.log_turn_score(turn_idx, "cluegiver invalid format", turn_score["cluegiver invalid format"])
+            self.log_turn_score(turn_idx, "guesser invalid format", turn_score["guesser invalid format"])
+            self.log_turn_score(turn_idx, "target precision", target_precision)
+            self.log_turn_score(turn_idx, "target recall", target_recall)
+            self.log_turn_score(turn_idx, "target f1", target_f1)
+            self.log_turn_score(turn_idx, "team precision", team_precision)
+
+            episode_score["number of turns"] += 1
+            for card_type in turn_score["cards uncovered"]:
+                episode_score["cards uncovered"][card_type] += turn_score["cards uncovered"][card_type]
+
+        # Metrics per game:
+        # win/lose
+        # if lose, lost through assassin or lost through other team being faster
+        # number of team cards revealed vs. all cards revealed by agent
+        # how many turns needed
+        # TODO: remove magic numbers 9 and 8, should come from card assignments somewhere
+        self.log_episode_score("win", episode_score["cards uncovered"]["team"] >= 9)
+        self.log_episode_score("lose through opponent", episode_score["cards uncovered"]["opponent"] >= 8)
+        self.log_episode_score("lose through assassin", episode_score["cards uncovered"]["assassin"] > 0)
+        self.log_episode_score("number of turns", episode_score["number of turns"])
+        self.log_episode_score("team cards revealed", episode_score["cards uncovered"]["team"] / 9)
+        self.log_episode_score("other cards revealed", 1 - (episode_score["cards uncovered"]["assassin"] + episode_score["cards uncovered"]["opponent"] + episode_score["cards uncovered"]["innocent"]) / 16)
+        # Main Score: 5% per team word uncovered, 5% if assassin covered, 1% for innocent covered, 2% for opponent card covered, 3% for target uncovered 
+        # TODO: reverse these scores of course! Apart from team cards and target cards!
+        main_score = 0
+        main_score += 0.05 * episode_score["cards uncovered"]["team"] + 0.03 * episode_score["cards uncovered"]["target"]
+        main_score += 0.05 * (1 -  episode_score["cards uncovered"]["assassin"])
+        main_score += 0.01 * (7 - episode_score["cards uncovered"]["innocent"]) + 0.02 * (8 - episode_score["cards uncovered"]["opponent"] - episode_score["number of turns"] + 1)
+        assert 0 <= main_score <= 100, f"Main Score of {main_score} is not between 0 and 100"
+        self.log_episode_score("Main Score", main_score)
 
 
 class CodenamesGameBenchmark(GameBenchmark):
