b7eb6f63ee5f5158ba1b6e6c230b8814a9e131f5
Philipp Sadler <philipp.sadler@gmail.com>
2024-02-09 09:57:37 +0100

    - add openai entries to model registry - remove text-davinci-003 (not listed anymore in https://api.openai.com/v1/models) - default to chat completions

--- backends/openai_api.py
+++ backends/openai_api.py
@@ -7,33 +7,19 @@ import backends
 
 logger = backends.get_logger(__name__)
 
-MODEL_GPT_4_1106_PREVIEW = "gpt-4-1106-preview"
-MODEL_GPT_4_0613 = "gpt-4-0613"
-MODEL_GPT_4_0314 = "gpt-4-0314"
-MODEL_GPT_35_1106 = "gpt-3.5-turbo-1106"
-MODEL_GPT_35_0613 = "gpt-3.5-turbo-0613"
-MODEL_GPT_3 = "text-davinci-003"
-SUPPORTED_MODELS = [MODEL_GPT_4_0314, MODEL_GPT_4_0613, MODEL_GPT_4_1106_PREVIEW, MODEL_GPT_35_1106, MODEL_GPT_35_0613, MODEL_GPT_3]
-
 NAME = "openai"
 
-MAX_TOKENS = 100   # 2024-01-10, das: Should this be hardcoded???
+MAX_TOKENS = 100  # 2024-01-10, das: Should this be hardcoded???
+
 
 class OpenAI(backends.Backend):
 
     def __init__(self, model_spec: backends.ModelSpec):
         super().__init__(model_spec)
         creds = backends.load_credentials(NAME)
-        if "organisation" in creds[NAME]:
-            self.client = openai.OpenAI(
-                api_key=creds[NAME]["api_key"],
-                organization=creds[NAME]["organisation"]
-                )
-        else:
-            self.client = openai.OpenAI(
-                api_key=creds[NAME]["api_key"]
-                )
-        self.chat_models: List = ["gpt-3.5-turbo-0613", "gpt-3.5-turbo-1106", "gpt-4-0314", "gpt-4-0613", "gpt-4-1106-preview"]
+        api_key = creds[NAME]["api_key"]
+        organization = creds[NAME]["organisation"] if "organisation" in creds[NAME] else None
+        self.client = openai.OpenAI(api_key=api_key, organization=organization)
 
     def list_models(self):
         models = self.client.models.list()
@@ -54,23 +40,15 @@ class OpenAI(backends.Backend):
                 ]
         :return: the continuation
         """
-        if self.model_spec.model_id in self.chat_models:
-            # chat completion
-            prompt = messages
-            api_response = self.client.chat.completions.create(model=self.model_spec.model_id,
-                                                          messages=prompt,
-                                                          temperature=self.get_temperature(),
-                                                          max_tokens=MAX_TOKENS)
-            message = api_response.choices[0].message
-            if message.role != "assistant":  # safety check
-                raise AttributeError("Response message role is " + message.role + " but should be 'assistant'")
-            response_text = message.content.strip()
-            response = json.loads(api_response.json())
+        prompt = messages
+        api_response = self.client.chat.completions.create(model=self.model_spec.model_id,
+                                                           messages=prompt,
+                                                           temperature=self.get_temperature(),
+                                                           max_tokens=MAX_TOKENS)
+        message = api_response.choices[0].message
+        if message.role != "assistant":  # safety check
+            raise AttributeError("Response message role is " + message.role + " but should be 'assistant'")
+        response_text = message.content.strip()
+        response = json.loads(api_response.json())
 
-        else:  # default (text completion)
-            prompt = "\n".join([message["content"] for message in messages])
-            api_response = self.client.completions.create(model=self.model_spec.model_id, prompt=prompt,
-                                                     temperature=self.get_temperature(), max_tokens=100)
-            response = json.loads(api_response.json())
-            response_text = api_response.choices[0].text.strip()
         return prompt, response, response_text
