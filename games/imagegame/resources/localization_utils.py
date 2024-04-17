2e73eaeebfdb67899b9c4612a50666838646d72f
Sandra Neuhäußer <sandra.neuhaeusser@gmx.net>
2024-04-17 09:58:17 +0200

    Add German translation for imagegame

--- games/imagegame/resources/localization_utils.py
+++ games/imagegame/resources/localization_utils.py
@@ -1,9 +1,9 @@
-LANGUAGES = {"en"}
+LANGUAGES = {"de", "en"}
 
 RESPONSE_PATTERNS = {"de":
-                         {"p1":'',
-                          "p2": '',
-                          "p1_terminate": ''},
+                         {"p1":'^anweisung: [^\n]+$',
+                          "p2": '^\n*([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n*$',
+                          "p1_terminate": '^\s*anweisung\s*:\s*fertig\s*$'},
                     "en":
                          {"p1":'^instruction: [^\n]+$',
                           "p2": '^\n*([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n*$',
