9faeb03ab1c69a90ec1509162c70d6873dea0dfe
Sandra Neuhäußer <sandra.neuhaeusser@gmx.net>
2024-06-14 09:46:55 +0200

    imagegame: Update localization_utils
    - only tag and terminate token contained
      - the patterns will be built in GameMaster
    - machine translations are treated as seperate languages
      - google translated resources have suffix '_google'
      - human translated have no suffix

--- games/imagegame/resources/localization_utils.py
+++ games/imagegame/resources/localization_utils.py
@@ -1,32 +1,26 @@
-LANGUAGES = {"de", "en"}
-
-RESPONSE_PATTERNS = {"de":
-                         {"p1":'^anweisung: [^\n]+$',
-                          "p2": '^\n*([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n*$',
-                          "p1_terminate": '^\s*anweisung\s*:\s*fertig\s*$'},
+MULTILINGUAL_PATTERNS = {
+                    "de":
+                         {"tag": 'anweisung:',
+                          "terminate_token": 'fertig'},
                     "en":
-                         {"p1":'^instruction: [^\n]+$',
-                          "p2": '^\n*([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n([A-Z▢]\s){4}[A-Z▢]\n*$',
-                          "p1_terminate": '^\s*instruction\s*:\s*done\s*$'},
-                    "es":
-                         {"p1":'',
-                          "p2": '',
-                          "p1_terminate": ''},
-                    "ru":
-                         {"p1":'',
-                          "p2": '',
-                          "p1_terminate": ''},
-                    "tk":
-                         {"p1":'',
-                          "p2": '',
-                          "p1_terminate": ''},
-                    "tr":
-                         {"p1":'',
-                          "p2": '',
-                          "p1_terminate": ''}
-,
-                    "te":
-                         {"p1":'',
-                          "p2": '',
-                          "p1_terminate": ''}
+                         {"tag": 'instruction:',
+                          "terminate_token": 'done'},
+                    "de_google":
+                         {"tag": 'anweisung:',
+                          "terminate_token": 'fertig'},
+                    "es_google":
+                         {"tag": 'instrucción:',
+                          "terminate_token": 'hecho'},
+                    "ru_google":
+                         {"tag": 'инструкция:',
+                          "terminate_token": 'готово'},
+                    "te_google":
+                         {"tag": 'సూచన:',
+                          "terminate_token": 'పూర్తయింది'},
+                    "tk_google":
+                         {"tag": 'görkezme:',
+                          "terminate_token": 'berildi'},
+                    "tr_google":
+                         {"tag": 'talimat:',
+                          "terminate_token": 'bi̇tti̇'}
                      }
