251925d9ddf2767df25e0d5c41ea54b8a8276a06
Sandra Neuhäußer <sandra.neuhaeusser@gmx.net>
2024-07-04 14:51:09 +0200

    imagegame: Ensure terminate pattern only matches first instruction (in case multiple instructions are generated)

--- games/imagegame/master.py
+++ games/imagegame/master.py
@@ -100,13 +100,17 @@ class ImageGameMaster(GameMaster):
 
         elif kind == "p1_response":
             instruction = ".+"
+            if self.mode == "liberal":
+                return f"^(?P<head>[\S\s]*?)(?P<body>(?P<tag>{tag})\s*(?P<instruction>{instruction}))(?P<tail>[\S\s]*)$"
+            elif self.mode == "strict":
+                return f"^(?P<head>)(?P<body>(?P<tag>{tag})\s*(?P<instruction>{instruction}))(?P<tail>)$"
+
         elif kind == "p1_terminate":
             instruction = MULTILINGUAL_PATTERNS[self.game.lang]["terminate_token"]
-
-        if self.mode == "liberal":
-            return f"^(?P<head>[\S\s]*?)(?P<body>(?P<tag>{tag})\s*(?P<instruction>{instruction}))(?P<tail>[\S\s]*)$"
-        elif self.mode == "strict":
-            return f"^(?P<head>)(?P<body>(?P<tag>{tag})\s*(?P<instruction>{instruction}))(?P<tail>)$"
+            if self.mode == "liberal":
+                return f"^(?P<head>((?!{tag})[\S\s])*)(?P<body>(?P<tag>{tag})\s*(?P<instruction>{instruction}))(?P<tail>[\S\s]*)$"
+            elif self.mode == "strict":
+                return f"^(?P<head>)(?P<body>(?P<tag>{tag})\s*(?P<instruction>{instruction}))(?P<tail>)$"
 
     @classmethod
     def applies_to(cls, game_name: str) -> bool:
