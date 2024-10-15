7095e7b6623e4cd8f796ea0a3761524c9a6f5377
Gnurro <knawurzelkopp@hotmail.de>
2024-10-15 14:17:44 +0200

    Change vLLM backend setup script setup_vllm.sh to not use pre-compiled wheels, now requires compiling binaries for installation; Change compilation note in requirements_vllm.txt warning about compiler requirement

--- setup_vllm.sh
+++ setup_vllm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 python3 -m venv venv_vllm
 source venv_vllm/bin/activate
-export VLLM_VERSION=0.5.4
 pip3 install -r requirements.txt
-pip3 install "transformers==4.43.2"
-pip3 install https://vllm-wheels.s3.us-west-2.amazonaws.com/nightly/vllm-${VLLM_VERSION}-cp38-abi3-manylinux1_x86_64.whl
+pip3 install -r requirements_vllm.txt
