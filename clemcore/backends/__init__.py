d15d26ed6539b5b4b5f87a4a7043c97631c0b043
Philipp Sadler <philipp.sadler@gmail.com>
2025-03-03 16:51:40 +0100

    allow to locate key.json at ~/.clemcore

--- clemcore/backends/__init__.py
+++ clemcore/backends/__init__.py
@@ -21,13 +21,19 @@ def load_credentials(backend, file_name="key.json") -> Dict:
     """Load login credentials and API keys from JSON file.
     Args:
         backend: Name of the backend/API provider to load key for.
-        file_name: Name of the key file. Defaults to key.json in the clembench root directory.
+        file_name: Name of the key file.
     Returns:
         Dictionary with {backend: {api_key: key}}.
     """
+
     key_file = os.path.join(os.getcwd(), file_name)
-    with open(key_file) as f:
-        creds = json.load(f)
+    try:  # first, optional location at cwd
+        with open(key_file) as f:
+            creds = json.load(f)
+    except:  # second, look into user home app dir
+        key_file = os.path.join(os.path.expanduser("~"), ".clemcore", file_name)
+        with open(key_file) as f:
+            creds = json.load(f)
     assert backend in creds, f"No '{backend}' in {file_name}. See README."
     assert "api_key" in creds[backend], f"No 'api_key' in {file_name}. See README."
     return creds
