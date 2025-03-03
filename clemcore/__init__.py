444d91dbc8ca8a738745f5633f3f49722823c101
Philipp Sadler <philipp.sadler@gmail.com>
2025-03-03 12:47:26 +0100

    allow to specify custom logging in cwd

--- clemcore/__init__.py
+++ clemcore/__init__.py
@@ -1,4 +1,6 @@
 import logging.config
+import os
+
 import yaml
 import importlib.resources as importlib_resources
 
@@ -20,7 +22,14 @@ print(BANNER)
 def load_logging_config():
     pkg_file_path = "utils/logging.yaml"
     with importlib_resources.files(__package__).joinpath(pkg_file_path).open("r") as f:
-        return yaml.safe_load(f)
+        default_config = yaml.safe_load(f)
+
+    custom_file = os.path.join(os.getcwd(), "logging.yaml")
+    if os.path.exists(custom_file):
+        with open(custom_file) as f:
+            custom_config = yaml.safe_load(f)
+        return {**default_config, **custom_config}
+    return default_config
 
 
 try:
