0c1a11190e79394d9db0780ae0690964043fbb00
Philipp Sadler <philipp.sadler@gmail.com>
2026-03-24 12:32:25 +0100

    Add testcase for branching only at first round and player role

--- tests/test_branching.py
+++ tests/test_branching.py
@@ -55,6 +55,7 @@ class TestBranchingRunner:
             InteractionsFileSaver(results_folder, player_model_infos=model_infos, store_branches=True),
             SignalFileSaver(results_folder)
         ])
+        # This results in 64 branches for each instance, by branching at each turn and playing 6 turns (2^6=64)
         branching.run(
             game_benchmark,
             game_instances,
@@ -62,4 +63,27 @@ class TestBranchingRunner:
             callbacks=callbacks,
             branching_factor=2,
             branching_condition=lambda **_: True
+        )
+
+    def test_branching_runner_with_results_folder_and_conditions(self, game_benchmark, game_instances, player_models):
+        results_folder = EpochResultsFolder(Path("results-branching"), Model.to_identifier(player_models))
+        model_infos = Model.to_infos(player_models)
+        callbacks = GameBenchmarkCallbackList([
+            EpochResultsFolderCallback(results_folder),
+            InstanceFileSaver(results_folder),
+            ExperimentFileSaver(results_folder, player_model_infos=model_infos),
+            InteractionsFileSaver(results_folder, player_model_infos=model_infos, store_branches=True),
+            SignalFileSaver(results_folder)
+        ])
+        # This results in 8 branches for each instance, by branching only at the first round on the describer's turn
+        branching.run(
+            game_benchmark,
+            game_instances,
+            player_models,
+            callbacks=callbacks,
+            branching_factor=8,
+            branching_condition=branching.combined_condition(
+                branching.is_player_role("WordDescriber"),
+                branching.is_round(0)
+            )
         )
